1e658df70e77ae87a52a9af2119744b2c58f780f
nrv23 <navemen23@hotmail.com>
2024-02-25 20:16:08 -0600

    seccion de web sockets

--- 14-websockets/src/app.ts
+++ 14-websockets/src/app.ts
@@ -1,4 +1,4 @@
-import { WebSocketServer } from 'ws';
+import { WebSocket, WebSocketServer } from 'ws';
 
 const wss = new WebSocketServer({ port: 3000 });
 
@@ -9,10 +9,32 @@ wss.on('connection', function connection(ws) {
     ws.on('error', console.error);
 
     ws.on('message', function message(data) {
-        console.log('received: %s', data);
+        console.log("Desde el cliente", data.toString());
+
+        const payload = {
+            type: "custom-message",
+            payload: {
+                message: data.toString()
+            }
+        }
+        //ws.send(JSON.stringify(payload));
+        //incluye a todos
+        wss.clients.forEach(function each(client) {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(JSON.stringify(payload), { binary: false });
+            }
+        });
+
+        //excluye al emisor del mensaje
+
+        /*wss.clients.forEach(function each(client) {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(JSON.stringify(payload), { binary: false });
+            }
+        });*/
+
     });
 
-    ws.send('Hola desde el servidor');
 
     ws.on("close", () => console.log("Desconectado"));
 });
